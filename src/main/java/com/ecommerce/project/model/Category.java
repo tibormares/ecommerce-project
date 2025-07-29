932814b261beb524392653fabf0423b830d38182
Tibor Mares <tibor.mares0@gmail.com>
2025-07-29 15:54:47 +0200

    Create first GET method

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,29 @@
+package com.ecommerce.project.model;
+
+public class Category {
+
+    private Long categoryId;
+    private String name;
+
+    public Category(Long categoryId, String name) {
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
