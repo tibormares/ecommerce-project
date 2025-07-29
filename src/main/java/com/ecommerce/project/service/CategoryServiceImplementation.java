a64ae3f8830053210d7f35e5bbe9cf008a6b374c
Tibor Mares <tibor.mares0@gmail.com>
2025-07-29 16:45:30 +0200

    Implement automatic Id for category

--- src/main/java/com/ecommerce/project/service/CategoryServiceImplementation.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImplementation.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class CategoryServiceImplementation implements CategoryService {
 
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -18,6 +19,7 @@ public class CategoryServiceImplementation implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
 
