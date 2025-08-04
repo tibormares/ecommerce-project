955c1c33f6d49cc1781f9c287e3182f669072941
Tibor Mares <tibor.mares0@gmail.com>
2025-08-04 16:29:52 +0200

    feat: Implement POST method for Product

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+
 public interface ProductService {
 
-
+    ProductDTO addProduct(Long categoryId, Product product);
 
 }
