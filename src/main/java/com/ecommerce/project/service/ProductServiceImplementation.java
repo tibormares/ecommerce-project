ef15eaece92cb89c903db4db5293453530c2adc6
Tibor Mares <tibor.mares0@gmail.com>
2025-08-06 11:36:07 +0200

    feat: Implement GET method for searching products by category id

--- src/main/java/com/ecommerce/project/service/ProductServiceImplementation.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImplementation.java
@@ -37,8 +37,23 @@ public class ProductServiceImplementation implements ProductService {
         return modelMapper.map(savedProduct, ProductDTO.class);
     }
 
+    @Override
     public ProductResponse getAllProducts() {
         List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .toList();
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+    }
+
+    @Override
+    public ProductResponse searchByCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        List<Product> products = productRepository.findByCategoryOrderByPriceAsc(category);
         List<ProductDTO> productDTOS = products.stream()
                 .map(product -> modelMapper.map(product, ProductDTO.class))
                 .toList();
