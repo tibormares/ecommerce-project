9986c6bc6f98663b5ed6f1f2b50004583572600a
Tibor Mares <tibor.mares0@gmail.com>
2025-08-15 17:50:12 +0200

    feat: Implement adding products to cart

---
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -0,0 +1,24 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity) {
+        CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
+        return new ResponseEntity<>(cartDTO, HttpStatus.CREATED);
+    }
+
+}
---
+++ src/main/java/com/ecommerce/project/service/CartService.java
@@ -0,0 +1,9 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.payload.CartDTO;
+
+public interface CartService {
+
+    CartDTO addProductToCart(Long productId, Integer quantity);
+
+}
