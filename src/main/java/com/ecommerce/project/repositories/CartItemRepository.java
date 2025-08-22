d1128a461aa6b9c88cf70b3521266d89b079eb4d
Tibor Mares <tibor.mares0@gmail.com>
2025-08-22 14:44:10 +0200

    fix(cart): Correct parameter order in cart item query

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
-    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    @Query("SELECT ci FROM CartItem ci WHERE ci.product.id = ?1 AND ci.cart.id = ?2")
     CartItem findCartItemByProductIdAndCartId(Long productId, Long cartId);
 
     @Modifying
