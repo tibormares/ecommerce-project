90f785d5230f44ea22abe0afea4d751a8acfef45
Tibor Mares <tibor.mares0@gmail.com>
2025-08-22 11:54:03 +0200

    feat: Add DTOs for order processing

---
+++ src/main/java/com/ecommerce/project/payload/OrderRequestDTO.java
@@ -0,0 +1,19 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequestDTO {
+
+    private Long addressId;
+    private String paymentMethod;
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
+}
