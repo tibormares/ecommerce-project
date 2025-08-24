cfd9fa80e2f27d4a0548b7ef2b2d496e865323f7
Tibor Mares <tibor.mares0@gmail.com>
2025-08-24 15:16:21 +0200

    docs(openapi): Add global API metadata

--- src/main/java/com/ecommerce/project/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/project/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +26,19 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot eCommerce API")
+                        .version("v1.0")
+                        .description("This is a Spring Boot project for eCommerce")
+                        .license(new License()
+                                .name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact()
+                                .name("Tibor Mareš")
+                                .email("private")
+                                .url("https://github.com/tibormares")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                        .url("https://github.com/tibormares/ecommerce-project"))
                 .components(new Components().
                         addSecuritySchemes("Bearer Authentication", bearerScheme))
                 .addSecurityItem(bearerRequirement);
