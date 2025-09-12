57f8982edc7369007083ebd65d69853b0eb7eb3e
Tibor Mares <tibor.mares0@gmail.com>
2025-09-12 17:32:32 +0200

    test(CategoryService): Add unit tests

---
+++ src/test/java/com/ecommerce/project/CategoryServiceImplementationTest.java
@@ -0,0 +1,134 @@
+package com.ecommerce.project;
+
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.repositories.CategoryRepository;
+import com.ecommerce.project.service.CategoryServiceImplementation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceImplementationTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private CategoryServiceImplementation categoryService;
+
+    @Test
+    void createCategory_shouldCreateCategory_whenCategoryNameIsUnique() {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setCategoryName("Kitchen");
+
+        Category category = new Category();
+        category.setCategoryName("Kitchen");
+
+        when(modelMapper.map(categoryDTO, Category.class)).thenReturn(category);
+        when(categoryRepository.findByCategoryName(category.getCategoryName())).thenReturn(null);
+        when(categoryRepository.save(category)).thenReturn(category);
+        when(modelMapper.map(category, CategoryDTO.class)).thenReturn(categoryDTO);
+
+        CategoryDTO createdCategoryDTO = categoryService.createCategory(categoryDTO);
+
+        verify(categoryRepository, times(1)).save(category);
+        assertEquals(categoryDTO, createdCategoryDTO);
+    }
+
+    @Test
+    void createCategory_shouldThrowException_whenCategoryNameIsPresent() {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setCategoryName("Kitchen");
+
+        Category category = new Category();
+        category.setCategoryName("Kitchen");
+
+        when(modelMapper.map(categoryDTO, Category.class)).thenReturn(category);
+        when(categoryRepository.findByCategoryName(category.getCategoryName())).thenReturn(category);
+
+        assertThrows(APIException.class, () ->
+                categoryService.createCategory(categoryDTO)
+        );
+    }
+
+    @Test
+    void deleteCategory_shouldDeleteCategory_whenCategoryExists() {
+        Long categoryId = 1L;
+        Category category = new Category();
+        category.setCategoryId(categoryId);
+        category.setCategoryName("Electronics");
+
+        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
+        when(modelMapper.map(category, CategoryDTO.class)).thenReturn(new CategoryDTO());
+
+        CategoryDTO deletedCategoryDTO = categoryService.deleteCategory(categoryId);
+
+        verify(categoryRepository, times(1)).delete(category);
+        assertNotNull(deletedCategoryDTO);
+    }
+
+    @Test
+    void deleteCategory_shouldThrowException_whenCategoryNotFound() {
+        Long nonExistentCategoryId = 999L;
+
+        when(categoryRepository.findById(nonExistentCategoryId)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () ->
+                categoryService.deleteCategory(nonExistentCategoryId)
+        );
+    }
+
+    @Test
+    void updateCategory_shouldUpdateCategory_whenCategoryFound() {
+        Long categoryId = 1L;
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setCategoryName("New Name");
+
+        Category categoryBeforeUpdate = new Category();
+        categoryBeforeUpdate.setCategoryName("Old Name");
+        categoryBeforeUpdate.setCategoryId(categoryId);
+
+        Category categoryAfterUpdate = new Category();
+        categoryAfterUpdate.setCategoryName("New Name");
+        categoryAfterUpdate.setCategoryId(categoryId);
+
+        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(categoryBeforeUpdate));
+        when(modelMapper.map(categoryDTO, Category.class)).thenReturn(categoryAfterUpdate);
+        when(categoryRepository.save(categoryBeforeUpdate)).thenReturn(categoryAfterUpdate);
+        when(modelMapper.map(categoryAfterUpdate, CategoryDTO.class)).thenReturn(categoryDTO);
+
+        CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
+
+        verify(categoryRepository, times(1)).save(categoryBeforeUpdate);
+        assertEquals("New Name", savedCategoryDTO.getCategoryName());
+    }
+
+    @Test
+    void updateCategory_shouldThrowException_whenCategoryNotFound() {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setCategoryName("Garage");
+
+        Long nonExistentCategoryId = 999L;
+
+        when(categoryRepository.findById(nonExistentCategoryId)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () ->
+                categoryService.updateCategory(categoryDTO, nonExistentCategoryId)
+        );
+    }
+
+}
